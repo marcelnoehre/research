317915cc62f2e430209d7263385c4798aaddeba4
marcelnoehre <marcelnoehre@web.de>
2026-06-04 10:02:01 +0200

    skip double irreducible nodes

--- draw/sublattices.py
+++
@@ -1,9 +0,0 @@
-from data import Parser
-from fcapy.lattice import ConceptLattice
-import networkx as nx
-
-file = 'Forum-Romanum'
-parser = Parser()
-cxt = parser.decode_cxt(f'../data/{file}.cxt')
-lat = ConceptLattice.from_context(cxt)
-G = nx.transitive_closure(lat.to_networkx())
